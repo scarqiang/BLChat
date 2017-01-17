b7f489efcd82193ce107f7ac43c11f50083dc372
HZQ <Qiang@HZQdeiMac.local>
2017-01-17 18:48:33 +0800

    创建项目

---
+++ BLChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLChat
+//
+//  Created by HZQ on 17/1/17.
+//  Copyright © 2017年 HZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
