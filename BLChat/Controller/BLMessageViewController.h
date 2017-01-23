02705ffc017266c9205b84b026e74bf2bb3fba5e
黄 泽宇 <huangzeyu@ibeiliao.com>
2017-01-23 17:55:00 +0800

    add message view controller

---
+++ BLChat/Controller/BLMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLMessageViewController.h
+//  BLChat
+//
+//  Created by 黄泽宇 on 1/23/17.
+//  Copyright © 2017 HZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLMessageViewController : ASViewController
+
+@end
---
+++ BLChat/Controller/BLMessageViewController.m
@@ -0,0 +1,13 @@
+//
+//  BLMessageViewController.m
+//  BLChat
+//
+//  Created by 黄泽宇 on 1/23/17.
+//  Copyright © 2017 HZQ. All rights reserved.
+//
+
+#import "BLMessageViewController.h"
+
+@implementation BLMessageViewController
+
+@end
